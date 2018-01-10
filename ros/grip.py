8b2589539c4639d2a8f6d7a282d44c69868a9e6b
Alexandra L LaGrassa <lagrassa@mit.edu>
2018-01-10 11:45:10 -0500

    grip.py successfully crimps the grip of the robot in simulation

--- ros/grip.py
+++ ros/grip.py
@@ -1,69 +1,20 @@
-#include <ros/ros.h>
-#include <pr2_controllers_msgs/Pr2GripperCommandAction.h>
-#include <actionlib/client/simple_action_client.h>
-//from http://wiki.ros.org/pr2_controllers/Tutorials/Moving%20the%20gripper
+import rospy
+from std_msgs.msg import String
+from pr2_controllers_msgs.msg import Pr2GripperCommand
 
-// Our Action interface type, provided as a typedef for convenience
-typedef actionlib::SimpleActionClient<pr2_controllers_msgs::Pr2GripperCommandAction> GripperClient;
+def gripper():
+    pub = rospy.Publisher('r_gripper_controller/command', Pr2GripperCommand, queue_size=10)
+    rospy.init_node('gripper', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        grip_msg = Pr2GripperCommand()
+        grip_msg.max_effort = -1
+        grip_msg.position = 0.06
+        pub.publish(grip_msg)
+        rate.sleep()
 
-class Gripper{
-private:
-  GripperClient* gripper_client_;  
-
-public:
-  //Action client initialization
-  Gripper(){
-
-    //Initialize the client for the Action interface to the gripper controller
-    //and tell the action client that we want to spin a thread by default
-    gripper_client_ = new GripperClient("r_gripper_controller/gripper_action", true);
-    
-    //wait for the gripper action server to come up 
-    while(!gripper_client_->waitForServer(ros::Duration(5.0))){
-      ROS_INFO("Waiting for the r_gripper_controller/gripper_action action server to come up");
-    }
-  }
-
-  ~Gripper(){
-    delete gripper_client_;
-  }
-
-  //Open the gripper
-  void open(){
-    pr2_controllers_msgs::Pr2GripperCommandGoal open;
-    open.command.position = 0.08;
-    open.command.max_effort = -1.0;  // Do not limit effort (negative)
-    
-    ROS_INFO("Sending open goal");
-    gripper_client_->sendGoal(open);
-    gripper_client_->waitForResult();
-    if(gripper_client_->getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-      ROS_INFO("The gripper opened!");
-    else
-      ROS_INFO("The gripper failed to open.");
-  }
-
-  //Close the gripper
-  void close(){
-    pr2_controllers_msgs::Pr2GripperCommandGoal squeeze;
-    squeeze.command.position = 0.0;
-    squeeze.command.max_effort = 50.0;  // Close gently
-    
-    ROS_INFO("Sending squeeze goal");
-    gripper_client_->sendGoal(squeeze);
-    gripper_client_->waitForResult();
-    if(gripper_client_->getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-      ROS_INFO("The gripper closed!");
-    else
-      ROS_INFO("The gripper failed to close.");
-  }
-};
-
-int main(int argc, char** argv){
-  ros::init(argc, argv, "simple_gripper");
-  Gripper gripper;
-  gripper.open();
-  gripper.close();
-
-  return 0;
-}
+if __name__ == '__main__':
+    try:
+        gripper()
+    except rospy.ROSInterruptException:
+        pass
