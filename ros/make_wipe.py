c826389f5493de68edcd5b140aa0ffe67866145c
Alexandra L LaGrassa <lagrassa@mit.edu>
2018-01-16 16:47:18 -0500

    alternating wiping motion

--- ros/make_wipe.py
+++ ros/make_wipe.py
@@ -1,11 +1,12 @@
 import rospy
 from geometry_msgs.msg import Point
 rospy.init_node("wiper")
-
 pub= rospy.Publisher('/rl_erase/wipe',Point,queue_size=10)
-pt = Point()
-pt.y = 0.1
-pub.publish(pt)
-rospy.sleep(2)
+numsteps = 30
+for i in range(numsteps):
+    pt = Point()
+    pt.y = 0.15*(-1)**i
+    pub.publish(pt)
+    rospy.sleep(5)
 
 
