3c98d978d0b4c5a57cf2d83a4c828d716b721c42
Alex LaGrassa <lagrassa@mit.edu>
2018-01-26 13:26:45 -0500

    added learning rate and switched to p erased

--- erase_controller_pkg/scripts/cv/masking.py
+++ erase_controller_pkg/scripts/cv/masking.py
@@ -5,24 +5,38 @@
 import numpy as np
 import cv2
 import pdb
-#test case:
-# prior  0.5
-# marker white
-marker = np.array([[0,255],[255,0]]).astype(np.uint8)
-white = np.array([[0,255],[0,255]]).astype(np.uint8)
-prior = np.array([[0.42,0],[0,0]])
 
 
-def p_marked(prior,marker, white):
+def p_marked(prior,marker, white, lr =1):
     not_white = cv2.bitwise_not(white)
     not_marker = cv2.bitwise_not(marker)
 
     uncertain = prior*cv2.bitwise_and(not_white, not_marker)/255
-    def_marker = cv2.bitwise_and(not_white, marker)/255
+    def_white = cv2.bitwise_and(white, not_marker)/255
     confused = 0.5*cv2.bitwise_and(marker, white)/255
-    return uncertain + def_marker + confused
+    
+    new =  uncertain + def_white + confused
+    return new*lr + prior*(1-lr)
 
-print p_marked(prior, marker, white)
+if __name__ == "__main__":
+    #run test
+    #test case:
+    # prior  0.5
+    # marker white
+    marker = np.array([[0,255],[255,0]]).astype(np.uint8)
+    white = np.array([[0,255],[0,255]]).astype(np.uint8)
+    prior = np.array([[0.42,0],[0,0]])
+    result = p_marked(prior, marker,white)
+    correct_result = np.array([[0.42,0.5],[0,1]])
+    if np.array_equal(result, correct_result):
+        print("lr = 1 test passed!")
+    else:
+        print "Your array ",result 
+        print "Correct array ",correct_result 
+
+    
+    
+
 
     
 
