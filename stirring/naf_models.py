6eee13df89bfe647c1bef3da3e5e8a49e5850e0d
Alex LaGrassa <lagrassa@mit.edu>
2018-04-15 21:21:36 -0400

    has a working implementation of NAF but with crazy vanishing gradients

--- stirring/naf_models.py
+++ stirring/naf_models.py
@@ -1,4 +1,5 @@
 from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy
+from keras.regularizers import l2
 from rl.memory import SequentialMemory
 from rl.core import Processor
 from rl.callbacks import FileLogger, ModelIntervalCheckpoint
@@ -6,8 +7,9 @@ import numpy as np
 import pdb
 import gym
 
+
 from keras.models import Sequential, Model
-from keras.layers import Dense, Activation, Flatten, Input, Concatenate
+from keras.layers import Dense, Activation, Flatten, Input, Concatenate, BatchNormalization
 from keras.optimizers import Adam
 
 from rl.agents import NAFAgent
@@ -27,48 +29,55 @@ class EmptyProcessor(Processor):
 
 def construct_agent(env, nb_actions, input_shape):
     V_model = Sequential()
+
     V_model.add(Flatten(input_shape=(1,) + input_shape))
-    V_model.add(Dense(16))
-    V_model.add(Activation('relu'))
-    V_model.add(Dense(16))
-    V_model.add(Activation('relu'))
-    V_model.add(Dense(16))
-    V_model.add(Activation('relu'))
-    V_model.add(Dense(1))
+    V_model.add(BatchNormalization())
+    V_model.add(Dense(16,kernel_regularizer=l2(0.00001), bias_regularizer=l2(0.00001)))
+    V_model.add(Activation('elu'))
+    V_model.add(Dense(16,kernel_regularizer=l2(0.00001), bias_regularizer=l2(0.00001)))
+    V_model.add(Activation('elu'))
+    V_model.add(Activation('elu'))
+    V_model.add(Dense(16,kernel_regularizer=l2(0.00001), bias_regularizer=l2(0.00001)))
+    V_model.add(Activation('elu'))
+    V_model.add(Activation('elu'))
+    V_model.add(Dense(1,kernel_regularizer=l2(0.00001), bias_regularizer=l2(0.00001)))
+    V_model.add(Activation('elu'))
     V_model.add(Activation('linear'))
     print(V_model.summary())
 
     mu_model = Sequential()
     mu_model.add(Flatten(input_shape=(1,) + input_shape))
-    mu_model.add(Dense(16))
-    mu_model.add(Activation('relu'))
-    mu_model.add(Dense(16))
-    mu_model.add(Activation('relu'))
-    mu_model.add(Dense(16))
-    mu_model.add(Activation('relu'))
-    mu_model.add(Dense(nb_actions))
+    mu_model.add(BatchNormalization())
+    mu_model.add(Dense(16,kernel_regularizer=l2(0.00001), bias_regularizer=l2(0.00001)))
+    mu_model.add(Activation('elu'))
+    mu_model.add(Dense(16,kernel_regularizer=l2(0.00001), bias_regularizer=l2(0.00001)))
+    mu_model.add(Activation('elu'))
+    mu_model.add(Dense(16,kernel_regularizer=l2(0.00001), bias_regularizer=l2(0.00001)))
+    mu_model.add(Activation('elu'))
+    mu_model.add(Dense(nb_actions,kernel_regularizer=l2(0.00001), bias_regularizer=l2(0.00001)))
     mu_model.add(Activation('linear'))
     print(mu_model.summary())
 
     action_input = Input(shape=(nb_actions,), name='action_input')
     observation_input = Input(shape=(1,) + input_shape, name='observation_input')
     x = Concatenate()([action_input, Flatten()(observation_input)])
-    x = Dense(32)(x)
-    x = Activation('relu')(x)
-    x = Dense(32)(x)
-    x = Activation('relu')(x)
-    x = Dense(32)(x)
-    x = Activation('relu')(x)
+    x = BatchNormalization()(x)
+    x = Dense(32,kernel_regularizer=l2(0.00001), bias_regularizer=l2(0.00001))(x)
+    x = Activation('elu')(x)
+    x = Dense(32,kernel_regularizer=l2(0.00001), bias_regularizer=l2(0.00001))(x)
+    x = Activation('elu')(x)
+    x = Dense(32,kernel_regularizer=l2(0.00001), bias_regularizer=l2(0.00001))(x)
+    x = Activation('elu')(x)
     x = Dense(((nb_actions * nb_actions + nb_actions) // 2))(x)
     x = Activation('linear')(x)
     L_model = Model(inputs=[action_input, observation_input], outputs=x)
     print(L_model.summary())
 
     processor = EmptyProcessor()
-    memory = SequentialMemory(limit=100000, window_length=1)
+    memory = SequentialMemory(limit=1000000, window_length=1)
     random_process = OrnsteinUhlenbeckProcess(theta=.15, mu=0., sigma=.3, size=nb_actions)
     agent = NAFAgent(nb_actions=nb_actions, V_model=V_model, L_model=L_model, mu_model=mu_model,
 		     memory=memory, nb_steps_warmup=100, random_process=random_process,
 		     gamma=.99, target_model_update=1e-3, processor=processor)
-    agent.compile(Adam(lr=.001, clipnorm=1.), metrics=['mae'])
+    agent.compile(Adam(lr=.0001, clipnorm=0.99, clipvalue=0.5), metrics=['mae'])
     return agent
--- stirring/rl_learn.py
+++ stirring/rl_learn.py
@@ -1,5 +1,6 @@
 from __future__ import division
 from scipy import misc
+import naf_models
 
 import numpy as np
 import pdb
@@ -11,7 +12,7 @@ from keras.layers import Dense, Activation, Flatten, Convolution2D, Permute, Inp
 from keras.optimizers import Adam
 import keras.backend as K
 
-from rl.agents.dqn import DQNAgent
+from rl.agents.dqn import DQNAgent, NAFAgent
 from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy
 from rl.memory import SequentialMemory
 from rl.core import Processor
@@ -20,81 +21,16 @@ from rl.callbacks import FileLogger, ModelIntervalCheckpoint
 
 WINDOW_LENGTH = 1
 ENV_NAME = "mix_cup"
+class Learner:
+    def __init__(self, env, nb_actions, input_shape):
+        self.dqn = naf_models.construct_agent(env, nb_actions, input_shape)
 
-#Currently implements the methods by returning what was given
-class EmptyProcessor(Processor):
-    def process_observation(self, observation):
-        #observation here is a board state, maybe do some form of 
-        #data augmentation at some point, but right now not going to
-        return observation
-    def process_state_batch(self, batch):
-        return batch
-    def process_reward(self, reward):
-        return reward
-
-
-class Learner():
-    def __init__(self, input_shape, window_length, nb_actions, dims=2):
-        picture_tensor = Input(shape=input_shape, dtype='float32', name="pictureTensor")
-        self.build_model(picture_tensor, input_shape, window_length, nb_actions, dims=dims)
-
-    def build_model(self, picture_tensor, input_shape, window_length, nb_actions, dims=2):
-        #here's where it's a bit weird. We have one part of the tuple,        #the input shape, but then the second part is the robot position 
-        net_input_shape =  (window_length,)+input_shape
-        HACK = True
-        if HACK:
-            grid =  Lambda(lambda x: x[:,:,:,0:3],  net_input_shape , dtype='float32')(picture_tensor)
-            #horrible hack: the top left corner
-	    robot =  Lambda(lambda x: x[:,:,:,3][:,0][:,0:dims],  (dims,) , dtype='float32')(picture_tensor) 
-	    #Convolution stuff
-	    grid = Conv2D(4,(2,2), activation='relu', padding='same')(picture_tensor)
-
-	    #grid = MaxPooling2D((3,3),strides=(1,1),padding='same')(grid)
-	    grid = Flatten(dtype='float32')(grid)
-	    #robot = Flatten(dtype='float32')(robot)
-	    fc1 = concatenate([robot, grid]) 
-	    action_tensor = Dense(nb_actions, activation = 'sigmoid', dtype='float32')(fc1)
-	    self.model = Model(inputs=[picture_tensor], outputs=action_tensor)
-        else:
-            model = Sequential()
-            print("net input shape", net_input_shape) 
-	    if K.image_dim_ordering() == 'tf':
-		# (width, height, channels)
-		#self.model.add(Permute((2,3, 1), input_shape= net_input_shape))
-		pass
-	    elif K.image_dim_ordering() == 'th':
-		# (channels, width, height)
-		self.model.add(Permute((1, 2, 3), input_shape=net_input_shape))
-	    else:
-		raise RuntimeError('Unknown image_dim_ordering.')
-            model.add(Convolution2D(12, 2, 2, subsample=(4, 4), input_shape = input_shape))
-	    model.add(Activation('relu'))
-	    model.add(Convolution2D(64, 3, 3, subsample=(1, 1)))
-	    model.add(Activation('relu'))
-	    model.add(Flatten())
-	    model.add(Dense(5))
-	    model.add(Activation('relu'))
-	    model.add(Dense(4))
-	    model.add(Activation('linear')) 
-            self.model = model
-	    
-
-        print(self.model.summary())
-
-    def train(self, env):
-        policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,nb_steps=5)
-        amount_memory = 1000000#0
-        memory = SequentialMemory(limit=amount_memory, window_length=WINDOW_LENGTH)
-        processor = EmptyProcessor()
-        self.dqn = DQNAgent(model=self.model, nb_actions=nb_actions, policy=policy, memory=memory, processor=processor, nb_steps_warmup=2, gamma=.90, target_model_update=5,train_interval=5, delta_clip=1., enable_double_dqn=False)
-        self.dqn.compile(Adam(lr=0.05), metrics=['mae'])
-
+    def train(self, env, nb_actions):
         weights_filename = 'dqn_{}_weights.h5f'.format(ENV_NAME)
         checkpoint_weights_filename = 'dqn_' + ENV_NAME + '_weights_{step}.h5f'
         log_filename = 'dqn_{}_log.json'.format(ENV_NAME)
         callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=2500)]
         callbacks += [FileLogger(log_filename, interval=100)]
-        #self.dqn.load_weights("dqn_shallow_learning_simple_board_imm_rew_weights_12500.h5f")
         self.dqn.fit(env, callbacks=callbacks, nb_steps=1750000, log_interval=10000, visualize=True, verbose=2)
            # After training is done, we save the final weights one more time.
         self.dqn.save_weights(weights_filename, overwrite=True)
@@ -106,11 +42,6 @@ class Learner():
         X, Y = load_supervised_data(actionfile = "actions.pkl", statefile = "states.pkl")
 
         #load image of robot and environment in 2 channels, call that X
-        #call the appropriate action to take y
-        #X = np.zeros((2, 10,10,2))
-        #X[0,0,0, 1] = 1;
-        #Y = [1, 2] 
-
         #initialize tensors
         self.model.fit(X, Y, epochs=90, batch_size=2) 
         #given the environment, predict the next step from the policy
@@ -121,12 +52,10 @@ class Learner():
 
     def test(self,env):
         policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,nb_steps=1000)
-        amount_memory = 10000000
+        amount_memory = 5000000
         memory = SequentialMemory(limit=amount_memory)
         processor = EmptyProcessor()
-        self.dqn = DQNAgent(model=self.model, nb_actions=nb_actions, policy=policy, memory=memory, processor=processor, nb_steps_warmup=50, gamma=.99, target_model_update=10000,train_interval=4, delta_clip=1., enable_double_dqn=False)
-        self.dqn.compile(Adam(lr=1), metrics=['mae'])
-        weights_filename = 'dqn_shallow_learning_complex_board_weights_25000.h5f'.format(ENV_NAME)
+        weights_filename = "placeholder"
         self.dqn.load_weights(weights_filename)
         self.dqn.test(env, nb_episodes=15, visualize=True)
 
@@ -157,11 +86,10 @@ def load_supervised_data(actionfile=None, statefile=None):
 # Finally, evaluate our algorithm for 10 episodes.
               
 if __name__=="__main__":
-     actions = [[1,0],[0,1],[-1,0],[0,-1]]
-     nb_actions = len(actions)
+     nb_actions = 2; #in our current model, we control x,y pos 
      env = StirEnv()
      state_shape = list(env.world_state.shape)
      robot_dims = env.robot_state.shape[0]
      state_shape[-1] +=1 
-     l = Learner((tuple(state_shape)),WINDOW_LENGTH,nb_actions, dims=robot_dims)
-     l.train(env)
+     l = Learner(env,nb_actions, tuple(state_shape))
+     l.train(env, nb_actions)
