3c1db9f933fcadffe0cb34307ef4a67c1319aa53
Alexandra L LaGrassa <lagrassa@mit.edu>
2018-05-09 20:02:42 -0400

    added hierarchical entropy, not sure if works yet

--- stirring/reward.py
+++ stirring/reward.py
@@ -1,3 +1,5 @@
+from __future__ import division
+from math import log
 #helper function: takes in a img and calculates the current reward
 #reward is based on the intended number of beads and the amount mixed
 import pdb
@@ -10,16 +12,9 @@ small_reward = False
 def reward_func(img, num_out):
     #calculate number of beads outside of cup
     #calculate mixedness
-    K1 = 1
-    K2 = 100
-    K3 = 0.1
-    
-    num_mixed, largest_unmixed = get_mixedness(img)
-    rew =  K1*num_mixed - K2*num_out-K3*largest_unmixed
+    rew = get_mixedness(img)
     return rew
 
-def get_out(img):
-    return 0
 
 def get_num_contours(hsv_filtered):
     _, binary_mask = cv2.threshold(hsv_filtered, 0.9,255, cv2.THRESH_BINARY)
@@ -50,36 +45,71 @@ def get_mixedness(img):
     #filter out the green stirrer! 
     img[:,:,1] = np.zeros(img[:,:,1].shape)
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) 
-    red  = cv2.inRange(hsv_img, np.array([115,0,0]),np.array([125,256,210])) #hack, this is okay because there are only two colors
-    blue  = cv2.inRange(hsv_img, np.array([-5,0,0]),np.array([5,256,210]))
-    contours_red, largest_red_section  = get_num_contours(red)
-    contours_blue, largest_blue_section  = get_num_contours(blue)
-    return contours_red+contours_blue, max(largest_red_section, largest_blue_section)
+    pdb.set_trace()
+    blue  = cv2.inRange(hsv_img, np.array([115,0,0]),np.array([125,256,210]))/255.0 #hack, this is okay because there are only two colors
+    red  = cv2.inRange(hsv_img, np.array([-5,0,0]),np.array([5,256,210]))/255.0
+    return hierarchical_entropy(red, blue)
 
+def hierarchical_entropy(red, blue):
+    #partition into 2, 4, 8 regions and compute entropy of subregions
+    #@requires 32x32 image or 64x64 image to work well, some multiple of 8
+    #I continue to think this is reasonable.
+    total_entropy = 0
+    for n in [2]:
+        total_entropy += n_entropy(red, blue, n)
+    return total_entropy
 
-def mixedness_region(red_section, blue_section):
+def showImage(red_section, blue_section):
+    img = np.zeros(red_section.shape+(3,), dtype = np.uint8)
+    img[:,:,0] = red_section*255
+    img[:,:,2] = blue_section*255
+    pdb.set_trace()
+    Image.fromarray(img).show()
+        
+
+def n_entropy(red, blue, n):
+    #split into n on the x and y axis
+    i_chunk_size = int(red.shape[0]/n)
+    j_chunk_size = int(red.shape[1]/n)
+    sum_entropy = 0
+    for i in range(n):
+        for j in range(n):
+            red_section = red[i*i_chunk_size:i*i_chunk_size+i_chunk_size, j*j_chunk_size:j*j_chunk_size+j_chunk_size]
+            blue_section = blue[i*i_chunk_size:i*i_chunk_size+i_chunk_size, j*j_chunk_size:j*j_chunk_size+j_chunk_size]
+            showImage(red_section, blue_section)
+            entropy = entropy_region(red_section, blue_section)
+            sum_entropy += entropy
+    return sum_entropy
+
+def entropy_region(red_section, blue_section):
     num_red = 0.0
     num_blue = 0.0
     try:
         num_red = sum(sum(red_section))
         num_blue = sum(sum(blue_section))
+        print("num_red", num_red, "num_blue", num_blue)
     except:
         pdb.set_trace()
-    if num_red == 0 and num_blue == 0:
-        #this region isn't worth bothering with
-        return 0
-    ratio = num_red/(num_red+num_blue+0.0)
-    #want to penalize distance from 0.5 more
-    k = 1.9 #some number between 1 and 2
-    return 1-k*abs(0.5-ratio)
+    p_red = num_red/(num_red+num_blue)
+    p_blue = num_blue/(num_red+num_blue)
+    print("p_red", p_red, "p_blue", p_blue)
+    #avoid math domain errors this way
+    entropy_individual = 0
+    if p_red > 0:
+        entropy_individual += p_red*log(p_red)
+    if p_blue > 0:
+        entropy_individual += p_blue*log(p_blue)
+
+    entropy = -(entropy_individual) 
+    return entropy
             
 if __name__ == "__main__":
     ims = ["all_blue.png","all_red.png", "nearly_blue.png", "nearly_red.png", "mix.png"]
-    ims = ["not-mixed.png"]
+    ims = ["stirred.png"]
     for im_name in ims:
         #im = Image.open(im_name).resize((10,10))
         im = cv2.imread(im_name)
-        im = cv2.resize(im, (200,200))
+        im = cv2.resize(im, (10,10))
     
         print(get_mixedness(im))
 
