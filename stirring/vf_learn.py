513c7aefe19cff627ed663dbe7614834bd1c0e88
Alex LaGrassa <lagrassa@mit.edu>
2018-05-10 22:18:30 -0400

    added better logging

--- stirring/vf_learn.py
+++ stirring/vf_learn.py
@@ -7,6 +7,7 @@ import numpy as np
 import pdb
 import pickle
 from stir_env import StirEnv
+import os
 
 from keras.models import Sequential, Model
 from keras.callbacks import CSVLogger
@@ -16,11 +17,19 @@ import keras.backend as K
 
 
 WINDOW_LENGTH = 1
-ENV_NAME = "mix_cup_5_10_2018"
+EXP_NAME = "mix_cup_5_10_2018"
+avg_l_fn = "average_length"+EXP_NAME+".py"
+avg_r_fn= "average_reward"+EXP_NAME+".py"
+for myfile in [avg_l_fn, avg_r_fn]:
+    if os.path.isfile(myfile):
+	os.remove(myfile)
+
+average_length_file = open(avg_l_fn,"a")
+average_reward_file = open(avg_r_fn,"a")
 class Learner:
     def __init__(self, env, nb_actions, input_shape, robot_dims):
         self.env = env
-        self.batch_size = 15
+        self.batch_size = 25
         self.rollout_size = 100
         self.input_shape = input_shape
         self.robot_dims = robot_dims
@@ -81,6 +90,8 @@ class Learner:
         robot_states = np.zeros((self.rollout_size, self.robot_dims))
         rewards = np.zeros(self.rollout_size)
         actions = np.zeros((self.rollout_size, self.nb_actions))
+        time_since_last_reset = 0
+        ep_times = []
         for i in range(self.rollout_size):
             #get current state
             img1, img2, robot_state = self.env.create_state()
@@ -95,28 +106,38 @@ class Learner:
 	    actions[i, :] = best_action
 	    rewards[i] = reward
             if i > 3 and episode_over:
+                ep_times.append(time_since_last_reset)
+                time_since_last_reset = 0
                 self.env.reset() #this is okay even though it's not a full rollout because we don't care about the state transitions, since this is super local
+            if len(ep_times) == 0: #congrats you didn't have to give up
+                ep_times.append(self.rollout_size)
+        #log the average V for the rollout, average length episode
+        average_length = sum(ep_times)/len(ep_times)
+        average_reward = sum(rewards)/len(rewards)
+        average_length_file.write(str(average_length)+",")
+        average_reward_file.write(str(average_reward)+",")
+    
         return img1s, img2s, robot_states, actions,rewards
             
             
 
     def train(self):
-        numsteps = 1000
+        numsteps = 10000
         SAVE_INTERVAL = 200
         # Load dataset
         #batch_size 25, takes 25 samples of states and actions, learn what the value should be after that
-        csv_logger = CSVLogger('log'+ENV_NAME+'.csv', append=True, separator=';')
-        self.model.load_weights('mix_cup_5_7_2018weights.h5f') #uncomment if you want to start from scratch
+        csv_logger = CSVLogger('log'+EXP_NAME+'.csv', append=True, separator=';')
+        #self.model.load_weights('mix_cup_5_7_2018weights.h5f') #uncomment if you want to start from scratch
         for i in range(numsteps):
             img1s, img2s, robot_states, actions, rewards = self.collect_batch() #collect batch using this policy
             self.model.fit([img1s, img2s, robot_states, actions], rewards, epochs=50, batch_size=self.batch_size, callbacks=[csv_logger]) 
             if i % SAVE_INTERVAL == 0:
-                self.model.save_weights(ENV_NAME+'_'+str(i)+'weights.h5f')
+                self.model.save_weights(EXP_NAME+'_'+str(i)+'weights.h5f')
 
 
     def test_supervised(self,env):
         self.model.compile(loss = "mean_absolute_error", optimizer='adam', metrics = ['accuracy'])
-        self.model.load_weights(ENV_NAME+'weights.h5f')
+        self.model.load_weights(EXP_NAME+'weights.h5f')
         X, Y = load_supervised_data(actionfile = "actions.pkl", statefile = "states.pkl")
         score = self.model.evaluate(X, Y);
         print("The score is")
