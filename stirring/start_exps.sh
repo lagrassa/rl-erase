97985287ba845d996dba05d3e7e0a5c106272a6d
Alex LaGrassa <lagrassa@mit.edu>
2018-07-10 15:37:20 -0400

    getting rid of cruft

--- stirring/start_exps.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-python pdg_learn.py --name "pdg_adagrad_1" &
-python pdg_learn.py --name "pdg_adagrad_2" &
-python pdg_learn.py --name "pdg_adagrad_3" &
-python pdg_learn.py --name "pdg_adagrad_4" &
-python pdg_learn.py --name "pdg_adagrad_5" &
-python pdg_learn.py --name "pdg_adagrad_6" &
-python pdg_learn.py --name "pdg_adagrad_7" &
-
